566396d895ee2f879343fe4a1af1e7f6811aa2cc
Ian Wootten <hi@niftydigits.com>
2018-02-05 12:39:55 +0000

    Add shot text to playblast (#27)
    * WIP: Add shot/frame to playblasts
    
    * Correct shot name lookup
    
    * Adjust comment to correct place

--- pyblish_bumpybox/plugins/maya/extract_playblast.py
+++ pyblish_bumpybox/plugins/maya/extract_playblast.py
@@ -1,5 +1,9 @@
 import pyblish.api
 from capture import capture
+import os
+import subprocess
+import importlib
+from bait.ftrack.query_runner import QueryRunner
 
 
 class BumpyboxMayaExtractPlayblast(pyblish.api.InstancePlugin):
@@ -12,15 +16,17 @@ class BumpyboxMayaExtractPlayblast(pyblish.api.InstancePlugin):
     hosts = ["maya"]
 
     def process(self, instance):
+        collection = instance.data["collection"]
 
-        filename = list(instance.data["collection"])[0]
-        filename = filename.replace(
-            instance.data["collection"].format("{tail}"),
-            ""
-        )
+        pipeline_env = os.getenv("PIPELINE_ENV", "dev")
+        settings = importlib.import_module("config.{}".format(pipeline_env))
+
+        filename_with_burnin = list(collection)[0]
+        collection_without_burnin = filename_with_burnin.replace(collection.format("{tail}"), "_notext")
+        filename_without_burnin = "{}.mov".format(collection_without_burnin)
 
         kwargs = {
-            'filename': filename,
+            'filename': collection_without_burnin,
             'viewer': False,
             'show_ornaments': False,
             'overwrite': True,
@@ -42,7 +48,41 @@ class BumpyboxMayaExtractPlayblast(pyblish.api.InstancePlugin):
         if 'audio' in instance.context.data and instance.context.data['audio']['enabled']:
             kwargs['sound'] = instance.context.data['audio']['node']
 
+        # Create quicktime from viewport settings
         capture(
             instance[0].getTransform().name(),
             **kwargs
         )
+
+        # Add text to the quicktime created
+        query_runner = QueryRunner()
+        task = query_runner.get_task(instance.context.data['ftrackData']['Task']['id'])
+        parents = query_runner.get_parents(task)
+
+        shotname = ""
+
+        for parent in parents:
+            if 'object_type' in parent and parent['object_type']['name'] == 'Shot':
+                shotname = parent['name']
+
+        query_runner.close_session()
+
+        network_tools_path = settings.networked_tools_dir
+        font_path = os.path.join(network_tools_path, "ffmpeg", "fonts", "OpenSans-Regular.ttf")
+        font_drive, font_tail = os.path.splitdrive(font_path)
+        font_path = "{}\\\\:/{}".format(font_drive.strip(":"), font_tail.strip("\\").replace("\\", "/"))
+
+        text_string = "[in]drawtext=fontfile={}:text='{}':x=50:y=50: fontcolor=white:fontsize=24, " \
+                      "drawtext=fontfile={}:text=%{{eif\\\\:n+1\\\\:d}}:x=50:y=(h-th)-50:fontcolor=white:fontsize=24[out]"\
+            .format(font_path, shotname, font_path)
+
+        ffmpeg_args = [
+            "{}\\ffmpeg\\bin\\ffmpeg.exe".format(network_tools_path), "-i", filename_without_burnin,
+            "-y",
+            "-vf", text_string,
+            "-crf", "1",
+            "-codec:a", "copy",
+            filename_with_burnin
+        ]
+
+        subprocess.call(ffmpeg_args)
